91dc6ab6cc367430d4ec76b793d8f0d14fb489a6
Himani Ahuja <himani.ahuja@sv.cmu.edu>
2012-07-18 09:42:01 -0700

    "first commit"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PMT::Application.config.secret_token = 'ee7a682e20e5d8bf2cfa03ac83a964a46c7a90b011897ea146c2b2197254af3bcf6c4af94f4b7ef876eb3439121ccc913d4ba534ded3715add9b842f12a47a6d'
